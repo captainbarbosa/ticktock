f206bd1d63e2b7055c1eacd230cec8f13482fe81
Nadia Barbosa <nadiabarbosa@me.com>
2016-05-03 21:18:53 -0400

    Add root route and define first view

---
+++ app/controllers/developers_controller.rb
@@ -0,0 +1,22 @@
+class DevelopersController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def updated
+  end
+
+  def destroy
+  end
+end
