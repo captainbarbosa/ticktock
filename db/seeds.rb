94f57e2a267107a1b96419d83bc884cc947f927a
Nadia Barbosa <nadiabarbosa@me.com>
2016-05-04 16:40:57 -0400

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+@developer = Developer.create(email: "dev@dev.com", password_hash: BCrypt::Password.create("password"), first_name: "Devy", last_name: "McDev")
+@project = Project.create(name: "Style app", max_allowed_hours: 10)
+TimeEntry.create(subject: "Wireframing", start_time: "2016-05-04 09:00:00", end_time: "2016-05-04 12:00:00", project_id: @project.id, developer_id: @developer.id)
+TimeEntry.create(subject: "HTML & CSS", start_time: "2016-05-04 13:00:00", end_time: "2016-05-04 14:00:00", project_id: @project.id, developer_id: @developer.id)
+TimeEntry.create(subject: "JavaScript", start_time: "2016-05-04 14:15:00", end_time: "2016-05-04 17:00:00", project_id: @project.id, developer_id: @developer.id)
