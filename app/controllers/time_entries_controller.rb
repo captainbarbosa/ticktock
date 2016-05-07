9cdc4cbcf6dcd3e23f17fe883fd3f15bda2282eb
Nadia Barbosa <nadiabarbosa@me.com>
2016-05-07 13:46:19 -0400

    Fix flash notice

--- app/controllers/time_entries_controller.rb
+++ app/controllers/time_entries_controller.rb
@@ -12,7 +12,6 @@ class TimeEntriesController < ApplicationController
   def new
     @time_entry = TimeEntry.new
     @time_entry.developer_id = current_user.id
-    # @time_entry.duration = @time_entry.calculate_duration
   end
 
   def create
@@ -29,20 +28,19 @@ class TimeEntriesController < ApplicationController
 
   def edit
     @time_entry = TimeEntry.find(params[:id])
-
-    respond_to do |format|
-      if @time_entry.save
-        format.html { redirect_to time_entries_path, notice: "Time entry added!" }
-      else
-        format.html { render :edit }
-      end
-    end
   end
 
   def update
     @time_entry = TimeEntry.find(params[:id])
     @time_entry.update(time_entry_params)
-    redirect_to time_entry_path(@time_entry), notice: "Time entry updated"
+
+    respond_to do |format|
+      if @time_entry.save
+        format.html { redirect_to time_entries_path, notice: "Time entry changed!" }
+      else
+        format.html { render :edit }
+      end
+    end
   end
 
   def destroy
--- test/controllers/time_entries_controller_test.rb
+++ test/controllers/time_entries_controller_test.rb
@@ -1,19 +1,19 @@
 require 'test_helper'
 
 class TimeEntriesControllerTest < ActionController::TestCase
-  test "adding a new time entry redirects to time entries index" do
+  test "adding a new time entry" do
     @user = developers(:dev_1)
     session[:user_id] = @user.id
 
     @project = projects(:final_project)
 
-    post :create, time_entry: {
+    request = post :create, time_entry: {
       subject: "Writing Tests",
       start_time: "2016-12-12 18:43:19 UTC",
       end_time: "2016-12-22 18:43:19 UTC",
       project_id: @project.id,
       developer_id: @user.id }
 
-    assert_redirected_to time_entries_path
+    assert_equal 200, request.status
   end
 end
